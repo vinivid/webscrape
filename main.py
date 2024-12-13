312bb113a7296c95c458569cfdc85a7137cb27f2
vini <frato.vini@gmail.com>
2024-12-12 22:52:03 -0300

    começo

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://www.cardinali.com.br/pesquisa-de-imoveis/?locacao_venda=L&id_cidade[]=190&finalidade=residencial&dormitorio=&garagem=&vmi=&vma=1.300%2C00&ordem=1&&pag=3')
+soup = BeautifulSoup(response.text, 'html.parser')
+
+all_blocks = soup.find_all('div', class_="muda_card1 ms-lg-0 col-12 col-md-12 col-lg-6 col-xl-4 mt-4 d-flex align-self-stretch justify-content-center")
+
+for element in all_blocks:
+    print(element.prettify())
